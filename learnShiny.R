5e2ab969ac91b7c6f97f0bcbed61341a0195f802
Leonardo S. Barone <leobarone@gmail.com>
2015-10-13 18:43:29 -0300

    Delete learnShiny.R

--- learnShiny.R
+++
@@ -1,8 +0,0 @@
-install.packages("shiny")
-dir.create("Preto_Cafe_App")
-dados <- data.frame("Id" = 0,
-                    "Nome" = "Eva",
-                    "Data" = "2015-07-27 23:59:59 BRT",
-                    "Produto" = "Gato escaldado")
-write.table(dados,"data/dados.txt", sep =  ";", row.names = F, quote = T)
-library(shiny)
